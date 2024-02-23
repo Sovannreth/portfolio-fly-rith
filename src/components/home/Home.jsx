c3a4f5991cbf94eea9f80714a72509a99a6dfff8
Sovannarith_ly <sovannarith.ly1405@gmail.com>
2024-02-23 14:02:58 +0700

    fix

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
 
                 <HeaderSocials />
                     <a href="#contact" className="btn">Here Me Now!</a>
+                    <a href="#contact" className="btn">Here You Now!</a>
+
                 {/* <ScrollDown /> */}
             </div>
             <Shapes />
