39eb86b07aaac83d022319f1e6b0083114bbecac
Sovannarith_ly <sovannarith.ly1405@gmail.com>
2024-03-14 09:59:10 +0700

    fix

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <section className="home container" id="home">
             <div className="intro">
                 <img src={Me} alt="" className="home__img" />
-                <h1 className="home__name">Pointer Property</h1>
+                <h1 className="home__name">Pointer Property CoL,td</h1>
                 <span className="home__education">I'm a Front-End Developer</span>
 
                 <HeaderSocials />
