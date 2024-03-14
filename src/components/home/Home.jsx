8151f491a41471df7727e52dd807dbefec2919b6
Sovannarith_ly <sovannarith.ly1405@gmail.com>
2024-03-14 09:57:24 +0700

    fix

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './home.css';
 import Me from '../../assets/avatar-1.svg';
 import HeaderSocials from './HeaderSocials';
-import ScrollDown from './ScrollDown';
+// import ScrollDown from './ScrollDown';
 import Shapes from "./Shapes";
 
 const Home = () => {
